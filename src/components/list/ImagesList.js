ec0ca9a8e248e6feba39f2134e5c4925c4090bea
Gerard Szulc <gerszulc05@gmail.com>
2018-06-12 17:20:46 +0200

    Deletion of unused Fragment import

--- src/components/list/ImagesList.js
+++ src/components/list/ImagesList.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from 'react'
+import React, {Component} from 'react'
 import IronImage from 'react-image-lazy-load-component';
 import VisibilitySensor from 'react-visibility-sensor'
 
